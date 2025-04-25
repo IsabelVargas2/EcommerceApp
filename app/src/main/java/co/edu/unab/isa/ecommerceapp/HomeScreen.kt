70e72d4451ca3f9d7c4e3bd00891000384225027
IsabelVargas2 <165979808+IsabelVargas2@users.noreply.github.com>
2025-04-24 19:42:35 -0500

    Cambio de iconos

--- app/src/main/java/co/edu/unab/isa/ecommerceapp/HomeScreen.kt
+++ app/src/main/java/co/edu/unab/isa/ecommerceapp/HomeScreen.kt
@@ -1,34 +1,130 @@
 package co.edu.unab.isa.ecommerceapp
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.edu.unab.isa.ecommerceapp.ui.theme.EcommerceAppTheme
+import coil3.compose.rememberAsyncImagePainter
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            val scrollBehavior =
+                TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+            MediumTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        "Bienvenido",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ExitToApp,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior
+            )
+        },
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text(
+                "Promociones", modifier = Modifier.padding(
+                    top = 16.dp,
+                    start = 16.dp,
+                    bottom = 8.dp
+                )
+            )
+            val listUrls = listOf(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt6p3jBsZXMywd-V_DheoS5OnEhaM-IclZNQ&s.jpg",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgwpgkOXl81FLdWaaFBi8oaeKOzhBwyC8N1w&s.jpg",
+                "https://previews.123rf.com/images/stickerside/stickerside2211/stickerside221100260/195938432-etiqueta-de-venta-plantilla-de-promoción-del-20-por-ciento-de-descuento-venta-de-marketing-de.jpg",
+                "https://img.freepik.com/vector-premium/plantilla-diseno-fondo-banner-venta-color-azul-naranja_500223-389.jpg",
+                "https://www.shutterstock.com/image-vector/summer-sale-promo-banner-clothes-260nw-1770029450.jpg"
+            )
+            LazyRow(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                contentPadding = PaddingValues(horizontal = 16.dp)
+            ) {
+                for (url in listUrls){
+                item { CardPromo(url)}
+                }
+            }
+        }
+    }
+}
+
 
 @Composable
-fun HomeScreen(){
-    Scaffold { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text("home sceen",
-                fontSize = 50.sp )
-        }
+fun CardPromo(url : String) {
+    Card(
+        modifier = Modifier
+            .height(180.dp)
+            .width(300.dp)
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt6p3jBsZXMywd-V_DheoS5OnEhaM-IclZNQ&s"),
+            contentDescription = "Promocion",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
     }
 
 }
 
 
-
-
 @Preview
 @Composable
-fun HomeScreenPreview(){
-    EcommerceAppTheme {
+fun HomeScreenPreview() {
+
         HomeScreen()
 
-    }
 }
